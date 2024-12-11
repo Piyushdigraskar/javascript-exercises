154a8a06412a530783e1a88d23f9ef5db05b7dc6
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-11 17:50:02 +0530

    js exercise 07 complete pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celVal = Math.floor((temp - 32)*5/9)/10;
+  return celVal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const farVal = Math.floor((temp * 9/5) + 32)/10;
+  return farVal;
 };
 
 // Do not edit below this line
