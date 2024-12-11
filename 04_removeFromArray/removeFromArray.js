fa1f58e5e147e5fc9be608bb3d4953ffc9e0c466
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-11 16:31:46 +0530

    js exercise 04 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+       let newarray = arr.filter((ele)=>{
+             return ele != num;
+       })
+       return newarray;
 };
 
 // Do not edit below this line
