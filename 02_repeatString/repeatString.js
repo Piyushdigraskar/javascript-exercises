8653074064979f24e67d03b08d0477b491c2c8b4
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-11 15:16:47 +0530

    js exercise 02 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(givenStr, num) {
+       let str = '';
+       if(givenStr === '' || num === 0){
+              return '';
+       }
+       if(num < 0) {
+              return "ERROR";
+       }
+       for(let i = 0; i < num;i++){
+              str += givenStr;
+       }
+       return str;
 };
 
 // Do not edit below this line
