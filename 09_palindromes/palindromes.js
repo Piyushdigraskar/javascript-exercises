b7b2cb7e03eaa9bb0fbba734aeea17aa474247e7
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-22 23:35:34 +0530

    palindrome complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+       const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+       const cleanedString = str
+       .toLowerCase()
+       .split('')
+       .filter(charcter => alphanumerical.includes(charcter))
+       .join();
+
+       const reversedString = str.split('').reverse().join();
+
+       return cleanedString === reversedString;
 };
 
 // Do not edit below this line
