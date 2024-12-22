ad70f99ea8a61931417650cf4d89df36b2d0ee16
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-12-22 23:24:12 +0530

    Calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sumOFArr = arr.reduce((acc, item)=> acc + item, 0);
+  return sumOFArr;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for(let i = n; i > 0;i--){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
